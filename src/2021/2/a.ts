a6664e26cb5c121aa405301429962fcc3044d0c9
piman51277 <piman.dev@gmail.com>
2021-12-02 10:21:06 -0500

    2021 - day 2

---
+++ src/2021/2/a.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+const movements = fs.readFileSync('./src/2021/2/input.txt', 'utf8').split('\n');
+let x = 0;
+let y = 0;
+for (const entry of movements) {
+    const [code, rawValue] = entry.split(' ');
+    const value = parseInt(rawValue);
+    switch (code) {
+        case "forward":
+            x += value;
+            break;
+        case "down":
+            y += value;
+            break;
+        case "up":
+            y -= value;
+            break;
+    }
+}
+console.log(x * y);
