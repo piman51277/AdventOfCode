f44daeedc5fefb9feaf7753f3a7e6b13757c7f78
piman51277 <piman.dev@gmail.com>
2021-12-02 10:37:16 -0500

    2015 - day 2

---
+++ src/2015/2/b.ts
@@ -0,0 +1,11 @@
+import fs from 'fs';
+const input = fs.readFileSync('./src/2015/2/input.txt', 'utf8').split('\n');
+function computePresentCost(x,y,z){
+    return 2*((x+y+z)-Math.max(x,y,z)) + (x*y*z);
+}
+let cost = 0;
+for(let i = 0; i < input.length; i++){
+    const [x,y,z] = input[i].split('x').map(Number);
+    cost += computePresentCost(x,y,z);
+}
+console.log(cost);
