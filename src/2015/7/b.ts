cd33c99db70be51f86c49402e7c7255f3aa3170b
piman51277 <piman.dev@gmail.com>
2021-12-03 15:57:19 -0500

    2015 - Day 7

--- src/2015/7/b.ts
+++ src/2015/7/b.ts
@@ -2,7 +2,7 @@ import {interpreter,executor,wires} from "./shared";
 
 //import commands and interpret
 import fs from "fs";
-const commands = fs.readFileSync('./src/2015/7/input.txt', 'utf8').split('\n');
+const commands = fs.readFileSync('./src/2015/7/input.txt', 'utf8').replace("1674 -> b","46065 -> b").split('\n');
 const code = commands.map(interpreter);
 for (let i = 0; i < code.length; i++) {
     for (let line = 0; line < code.length; line++) {
