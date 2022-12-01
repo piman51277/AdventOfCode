f14cf4a5b0b32d5394c7da68cf221d651e09f5cc
piman51277 <piman.dev@gmail.com>
2022-12-01 11:06:40 -0500

    reorgainize project

--- src/2015/1/a.ts
+++
@@ -1,3 +0,0 @@
-import fs from 'fs';
-const input = fs.readFileSync('./src/2015/1/input.txt', 'utf8');
-console.log(input.match(/\(/g).length - input.match(/\)/g).length);
--- src/2021/8/a.ts
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const input = fs.readFileSync('src/2021/8/input.txt', 'utf8').split(/\r?\n/);
-const parsed = input.map(line => line.split(' | ').map(s=>s.split(' ')));
-const count = parsed.map(entry => entry[1].filter(s => [2,3,4,7].includes(s.length)).length).reduce((a,b) => a+b);
-console.log(count);
