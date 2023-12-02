9d2c2381eff0dbd626bd1404e9116cbe5acb3d02
piman51277 <piman.dev@gmail.com>
2023-12-02 00:15:56 -0500

    2023 day 2

---
+++ solutions/2023/2/index2.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf8").split("\n");
+input.pop()
+
+
+let validnames = ["red", "green", "blue"]
+
+let count = 0
+for (let line of input) {
+
+  //get the game id
+  const gameid = parseInt(line.split(" ")[1])
+
+  //remove Game %d: from line
+  line = line.replace(/Game \d+: /g, "")
+
+  //separate by semicolon
+  cases = line.split(";")
+
+
+  let colorFreq = {
+    "red": 0,
+    "green": 0,
+    "blue": 0
+  }
+  for (let _case of cases) {
+    const items = _case.split(",")
+
+
+    for (let item of items) {
+      //is it red, green or blue?
+      for (let validname of validnames) {
+        if (item.includes(validname)) {
+          //remove the color from the item
+          item = item.replace(validname, "")
+
+          //remove any commas
+          item = item.replace(",", "")
+
+          //remove spaces
+          item = item.replace(" ", "")
+
+          //parse the number
+          const num = parseInt(item)
+
+
+          //add to the color frequency (max)
+          if (colorFreq[validname] === undefined) {
+            colorFreq[validname] = num
+          } else {
+            colorFreq[validname] = Math.max(colorFreq[validname], num)
+          }
+
+
+          break
+        }
+      }
+    }
+
+  }
+
+
+  count += colorFreq["red"] * colorFreq["green"] * colorFreq["blue"]
+}
+console.log(count)
