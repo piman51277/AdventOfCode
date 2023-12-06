75453f92f0b9dc655cdcd5f1098cba24b1e8b017
piman51277 <piman.dev@gmail.com>
2023-12-05 20:13:09 -0500

    increase speed

--- solutions/2023/5/index2.js
+++ solutions/2023/5/index2.js
@@ -65,7 +65,7 @@ for (const pair of pairs) {
 
   let minLoc = Infinity
   let minLocSeed = seed1
-  for (let i = seed1; i <= seed2; i += 1000) {
+  for (let i = seed1; i <= seed2; i += 50000) {
     const loc = getLocationofSeed(i)
     if (loc < minLoc) {
       minLoc = loc
@@ -74,7 +74,6 @@ for (const pair of pairs) {
   }
 
   minLocInPair.push([minLoc, minLocSeed, seed1, seed2])
-  console.log("FINSIHED A PAIR")
 }
 
 //get the min seed
@@ -89,7 +88,7 @@ for (const [loc, seed, seed1, seed2] of minLocInPair) {
 }
 
 //look +- 10000 from the min seed
-let delta = 100000
+let delta = 25000
 let minSearch = Math.max(range[0] - delta, range[1])
 let maxSearch = Math.min(range[0] + delta, range[2])
 
