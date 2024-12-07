c8f867005e4e4436b03077b92a02138422a3b2fd
piman51277 <piman@piman.dev>
2024-12-07 00:08:48 -0500

    day 7 537/331

---
+++ solutions/2024/7/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf8").split("\n");
+input.pop();
+
+function evaluateString(nums, ops) {
+  let result = nums[0];
+  for (let i = 0; i < ops.length; i++) {
+    if (ops[i] === "+") {
+      result += nums[i + 1];
+    } else {
+      result *= nums[i + 1];
+    }
+  }
+  return result;
+}
+
+let ok = 0;
+
+for (const line of input) {
+  const [test, rest] = line.split(": ");
+  const nums = rest.trim().split(" ").map(Number);
+
+  const possibleOps = ["+", "*"];
+  for (let i = 0; i < Math.pow(2, nums.length - 1); i++) {
+    const binary = i.toString(2).padStart(nums.length - 1, "0");
+    const ops = binary.split("").map((x) => possibleOps[x]);
+    //console.log(test, nums, ops, evaluateString(nums, ops));
+    if (evaluateString(nums, ops) === parseInt(test)) {
+      ok += parseInt(test);
+      break;
+    }
+  }
+}
+
+console.log(ok);
