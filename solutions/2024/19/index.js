27abfdbcb722e5382613b8d293e8eaa3dbe63a51
piman51277 <piman@piman.dev>
2024-12-20 00:17:36 -0500

    19

--- solutions/2024/19/index.js
+++ solutions/2024/19/index.js
@@ -1,5 +1,40 @@
 const fs = require("fs");
-const input = fs.readFileSync("input.txt", "utf8").split("\n");
-input.pop();
-for (const line of input) {
+const [fInput, sInput] = fs
+  .readFileSync("input.txt", "utf8")
+  .trim()
+  .split("\n\n");
+
+const patterns = fInput.split(", ");
+const designs = sInput.split("\n");
+
+function isPossible(design) {
+  function recurse(startIndex) {
+    if (startIndex === design.length) {
+      return true;
+    }
+    for (const pattern of patterns) {
+      //check if the design would fit
+      if (
+        design.slice(startIndex).startsWith(pattern) &&
+        design.length >= startIndex + pattern.length
+      ) {
+        //recurse
+        if (recurse(startIndex + pattern.length)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  return recurse(0);
 }
+
+let possible = 0;
+for (const design of designs) {
+  if (isPossible(design)) {
+    possible++;
+  }
+}
+
+console.log(possible);
--- solutions/2024/19/index2.js
+++ solutions/2024/19/index2.js
@@ -1,5 +1,47 @@
 const fs = require("fs");
-const input = fs.readFileSync("input.txt", "utf8").split("\n");
-input.pop();
-for (const line of input) {
+const [fInput, sInput] = fs
+  .readFileSync("input.txt", "utf8")
+  .trim()
+  .split("\n\n");
+
+const patterns = fInput.split(", ");
+const designs = sInput.split("\n");
+
+function isPossible(design) {
+  let cache = {};
+
+  function recurse(startIndex) {
+    if (cache[startIndex] !== undefined) {
+      console.log("cache hit", startIndex, cache[startIndex]);
+      return cache[startIndex];
     }
+
+    if (startIndex === design.length) {
+      return true;
+    }
+    let oks = 0;
+    for (const pattern of patterns) {
+      if (
+        design.slice(startIndex).startsWith(pattern) &&
+        design.length >= startIndex + pattern.length
+      ) {
+        const val = recurse(startIndex + pattern.length);
+        oks += val;
+      }
+    }
+    cache[startIndex] = oks;
+    console.log("cache set", startIndex, oks);
+    return oks;
+  }
+
+  return recurse(0);
+}
+
+let possible = 0;
+for (const design of designs) {
+  const k = isPossible(design);
+  possible += k;
+  console.log(design, k);
+}
+
+console.log(possible);
